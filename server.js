15989f7e8b82ed6752210cf81043a272ab6eb56d
ogMav8 <brad.anderson019@gmail.com>
2021-03-30 20:57:57 -0600

    add process.env.PORT

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const { animals } = require('./data/animals');
-
+const PORT = process.env.PORT || 3001
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
     console.log(req.query)
     res.json(results);
   });
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
   });
   
